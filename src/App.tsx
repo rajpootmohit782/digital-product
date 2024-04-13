a2e552fff4780013f4542a72d3dbf4292cf4e072
mohit <84062871+rajpootmohit782@users.noreply.github.com>
2024-04-13 20:55:39 +0000

    front almost completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import { useState } from 'react';
-import Header from './components/Header.jsx';
-import Homepage from './components/Homepage.jsx';
-import './App.css';
+import { useState } from "react";
+import Header from "./components/Header.jsx";
+import Homepage from "./components/Homepage.jsx";
+import Footer from "./components/Footer.jsx";
+import Marketing from "./components/Marketing.jsx";
+import Trusted from "./components/Trusted.jsx";
+import Review from "./components/Review.jsx";
+import Design from "./components/Design.jsx";
+import FrequentQuestions from "./components/FrequentQuestions.jsx";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +16,13 @@ function App() {
     <>
       <Header />
       <Homepage />
+      {/* <Img /> */}
+      <Marketing />
+      <Trusted />
+      <Review />
+      <Design />
+      <FrequentQuestions />
+      <Footer />
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 
 function Header() {
   return (
-    <header className="fixed w-full">
+    <header className="relative w-full">
       <nav className="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
         <div className="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
           <a href="#" className="flex items-center">
-            <img
-              src="./images/logo.svg"
-              className="h-6 mr-3 sm:h-9"
-              alt="Landwind Logo"
-            />
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Landwind
-            </span>
+            <img src="/logo.png" className="h-12 w-auto mr-8" alt="Logo" />
+
+            {/* Increase h-16 to make the logo bigger */}
+            {/* <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+        <span style={{ color: "red", fontSize: "24px" }}>tech</span>
+        <span style={{ color: "blue", fontSize: "24px" }}>Ram</span>
+    </span> */}
           </a>
+
           <div className="flex items-center lg:order-2">
             <div className="hidden mt-2 mr-4 sm:inline-block">
               <a
@@ -25,7 +25,7 @@ function Header() {
                 data-show-count="true"
                 aria-label="Star themesberg/landwind on GitHub"
               >
-                Star
+                Have a idea
               </a>
             </div>
             {/*<a href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a>*/}
@@ -33,7 +33,7 @@ function Header() {
               href="https://themesberg.com/product/tailwind-css/landing-page"
               className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800"
             >
-              Download
+              Book Meeting
             </a>
             <button
               data-collapse-toggle="mobile-menu-2"
